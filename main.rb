c13bde42e6e081f592f9c21367e4d1c3a78f6545
Kazutoyo Sasamoto <sasamotokazutoyo@gmail.com>
2023-03-07 15:13:44 +0900

    chaged map

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 require "dxruby"
 require_relative "player"
-require_relative "soko"
 require_relative "map"
 
 Window.width = 600
@@ -8,8 +7,11 @@ Window.height = 400
 Window.bgcolor = C_WHITE
 
 player = Player.new
+map = Map.new
 
 Window.loop do
+    map.draw
+
     player.draw
     player.move
 end
