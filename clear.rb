b50d58edfeabf26bc7e1db1fd894901fc2900f54
sutani <sutani.moeka0201@gmail.com>
2023-03-07 17:31:52 +0900

    add gamescreen

--- clear.rb
+++ clear.rb
@@ -1,1 +1,25 @@
 #クリア画面
+=begin
+require 'dxruby'
+
+#Window.width = 600
+#Window.height = 400
+
+image = Image.load('5.1.png')
+
+Window.loop dos
+    Window.draw(100,100,image)
+    
+end
+=end
+
+require 'dxruby'
+
+font = Font.new(32)
+
+Window.loop do
+    Window.draw_font(230,200,"GAME CLEAR",font)
+
+end
+
+
