c8c27163a151bf721a098a09f4f2e0291f61f1bb
iwatanabee <issei.watanabe0708@gmail.com>
2023-03-07 15:03:39 +0900

    change player

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require "dxruby"
 require_relative "player"
-require_relative "soko"
+# require_relative "soko"
 require_relative "map"
 
 Window.width = 600
@@ -8,8 +8,10 @@ Window.height = 400
 Window.bgcolor = C_WHITE
 
 player = Player.new
+map = Map.new
 
 Window.loop do
     player.draw
     player.move
+    map.draw
 end
--- player.rb
+++ player.rb
@@ -1,8 +1,12 @@
 class Player < Sprite
     def initialize
-        self.image = Image.new(30,60,C_BLUE)
+        @right_image = Image.load("images/hito_right.png")
+        @left_image = Image.load("images/hito_left.png")
+        self.image = @right_image
         self.x = 200
         self.y = 300 - self.image.height
+        @dx = 0
+        @speed = 4
     end
 
     def fall
@@ -10,7 +14,15 @@ class Player < Sprite
     end
 
     def move
-        self.x += 2 if Input.key_down?(K_D)
-        self.x -= 2 if Input.key_down?(K_A)
+        self.x += @dx
+        @dx = 0
+        @dx = @speed if Input.key_down?(K_D)
+        @dx = -@speed if Input.key_down?(K_A)
+
+        if @dx > 0
+            self.image = @right_image
+        elsif @dx < 0
+            self.image = @left_image
+        end
     end
 end
