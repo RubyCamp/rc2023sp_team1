1a5bd99702f1588713d6a19d8d6f8660f11dc2ef
Kazutoyo Sasamoto <sasamotokazutoyo@gmail.com>
2023-03-08 11:28:23 +0900

    map noncompleted

--- block.rb
+++ block.rb
@@ -1,11 +1,11 @@
 class Block < Sprite
-    def initialize
+    def initialize(x,y)
         @block_width = 80
         @block_height = 80
         self.image = Image.new(@block_width,@block_height,[100,0,0,0])
         self.collision = [0, 0, @block_width - 1, @block_height - 1]
-        self.x = 200
-        self.y = 200 - self.image.height
+        self.x = x
+        self.y = y
     end
 
     def move
--- main.rb
+++ main.rb
@@ -12,25 +12,23 @@ Window.bgcolor = C_WHITE
 player = Player.new
 map = Map.new
 door = Door.new(0,0,200,100,player)
-block = Block.new
 
 Window.loop do
     map_x = (player.x / 20).abs
     map_y = ((player.y + 50) / 20).abs
-    block_x = (block.x / 20).abs
-    block_y = ((block.y + 80) / 20).abs
+    # block_x = (map.move_block.x / 20).abs
+    # block_y = ((map.move_block.y + 80) / 20).abs
     player_gravity = Gravity.new(map_x,map_y,player)
-    block_gravity = Gravity.new(block_x,block_y,block)
+    # block_gravity = Gravity.new(block_x,block_y,map.move_block)
     player.draw
     player.move
     map.draw
     door.draw
-    block.draw
     player_gravity.fall
-    block_gravity.fall
-    arr = [player,block]
-    if Sprite.check(arr) then
+    # block_gravity.fall
+    # arr = [player,block]
+    # if Sprite.check(arr) then
         player.push
-        block.move
-    end
+        # block.move
+    # end
 end
