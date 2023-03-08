af25d0a133114f902084a9f4b112b6abe3f43892
iwatanabee <issei.watanabe0708@gmail.com>
2023-03-09 02:22:50 +0900

    add 3jump

--- gravity.rb
+++ gravity.rb
@@ -12,6 +12,9 @@ class Gravity
         line = @map.map_data[@map_y]
         if line[@map_x] == 0
             @object.y += 2
+            # @object.v = 0
+        # elsif line[@map_x] == 1
+        #     @object.v = -10
         end
-    end   
+    end
 end
--- player.rb
+++ player.rb
@@ -1,14 +1,24 @@
+require_relative "map"
+
 class Player < Sprite
+    attr_accessor :dy, :v
     def initialize
         @right_image = Image.load("images/hito_right.png")
         @left_image = Image.load("images/hito_left.png")
         self.image = @right_image
         self.x = 0
         self.y = 200 - self.image.height
+        @bottom = y + self.image.height
         self.collision = [0, 0, self.image.height - 1, self.image.width - 1]
         @dx = 0
         @dy = 0
+        @v = 0
         @speed = 4
+        @space_count = 0
+        @jump_count = 3
+        # @map_x = (self.x / 20).abs
+        # @map_y = ((self.y + 50) / 20).abs
+        # @map = Map.new
     end
 
     def move
@@ -19,8 +29,7 @@ class Player < Sprite
         
         self.y -= @dy
         @dy = 0
-        @dy = 10 if Input.key_down?(K_SPACE)
-
+        
         if @dx > 0
             self.image = @right_image
         elsif @dx < 0
@@ -28,7 +37,35 @@ class Player < Sprite
         end
     end
 
-    def push
+    def push 
+    end
+
+    def jump
+        # line = @map.map_data[@map_y]
+        if @space_count < 3
+            if Input.key_push?(K_SPACE)
+                @v = -10
+                @space_count += 1
+            end
+        end
+        @bottom += @v
+        # ここの条件をかえる
+        # もしも下のブロックが1の時spaceを0にしたい
+        if (@bottom >= 191)
+            @bottom = 190
+            @v = 0
+            @space_count = 0
+        end
+        # if line[@map_x] == 1
+        #     # @bottom = 
+        #     @v = 0
+        #     @space_count = 0
+        # end
+        # if line[@map_x] == 1
+        #     @space_count = 0
+        # end
+        self.y = @bottom - 32
+        @v += 1 # 重力加速度
         
     end
 end
