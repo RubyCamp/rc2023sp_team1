41e92340203e0ba1ea4c4791211dde46db8a2a4d
sutani <sutani.moeka0201@gmail.com>
2023-03-08 10:17:39 +0900

    def

--- clear.rb
+++ clear.rb
@@ -1,25 +1,11 @@
-#クリア画面
-=begin
-require 'dxruby'
 
-#Window.width = 600
-#Window.height = 400
-
-image = Image.load('5.1.png')
-
-Window.loop dos
-    Window.draw(100,100,image)
-    
-end
-=end
+## クリア画面
 
 require 'dxruby'
 
-font = Font.new(32)
-
-Window.loop do
-    Window.draw_font(230,200,"GAME CLEAR",font)
-
+def clear_action 
+    font = Font.new(32)
+    Window.loop do
+        Window.draw_font(230,200,"GAME CLEAR",font)
+    end
 end
-
-
