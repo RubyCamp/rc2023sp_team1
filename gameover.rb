44bf1da85a7fa8dbcbabe911a5e36c0e7846cfd2
sutani <sutani.moeka0201@gmail.com>
2023-03-08 10:03:52 +0900

    def

--- gameover.rb
+++ gameover.rb
@@ -1,10 +1,10 @@
 require 'dxruby'
 
-font = Font.new(32)
-
-Window.loop do
-    Window.draw_font(230,200,"GAME OVER",font)
-    Window.draw_font(150,250,"スペースキーを押してください",font)
-
-    break if Input.key_push?(K_SPACE)
+def gameover_action
+    font = Font.new(32)
+    Window.loop do
+        Window.draw_font(230,200,"GAME OVER",font)
+        Window.draw_font(150,250,"スペースキーを押してください",font)
+        break if Input.key_push?(K_SPACE)
+    end
 end
